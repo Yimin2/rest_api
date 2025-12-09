5b3f358e2e92e30373ff2b0ec752e55e00b6a7e0
Yimin2 <gownfla14@naver.com>
2025-12-09 11:50:21 +0900

    :sparkles: feat(security): JWT 인증 필터 구현
    - `JwtAuthenticationFilter` 클래스 추가로 요청당 JWT 인증 처리
      - 토큰 유효성 검증 및 사용자 정보 조회 로직 포함
      - SecurityContext에 인증 객체 저장 기능 구현
    - SecurityContextHolder 통해 인증 상태 관리
    - Authorization 헤더에서 Bearer 토큰 추출 메서드 추가

---
+++ src/main/java/com/ll/restapi/security/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.ll.restapi.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String token = resolveToken(request);
+            if (StringUtils.hasText(token) && jwtProvider.validateToken(token)) {
+                String username = jwtProvider.getUsername(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                // 인증 객체 생성
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // SecurityContext에 인증 객체 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                System.out.println("인증 완료");
+            }
+
+        } catch (Exception e) {
+            System.out.println("인증 실패");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
